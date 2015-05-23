02a1f7a2b1fcab551099dc3db29f9c2db7baefe5
MattReisman <mattreisman@gmail.com>
2015-05-23 15:40:15 -0600

    Use device session for api request

--- app/led.js
+++ app/led.js
@@ -3,29 +3,42 @@ if (Meteor.isClient) {
   Session.setDefault('lightState', 0);
 
   Template.led.helpers({
+    device: function() {
+      return Session.get('deviceID');
+    },
+    token: function() {
+      return Session.get('accessToken');
+    },
     lightState: function () {
-      Meteor.http.get("https://api.particle.io/v1/devices/53ff70066667574834302067/lights?access_token=dcb98d8057f3a74e2234d4a44a80cbfbafb3d183", function (err, data ){
-        console.log(data.data.result );
-        Session.set('lightState', data.data.result);
-      });
+      var device = Session.get('deviceID');
+      var token = Session.get('accessToken');
+      Meteor.http.get(
+        "https://api.particle.io/v1/devices/" +
+        device +
+        "/lights?access_token=" +
+       token,
+        function (err, data ){
+          console.log(data.data.result );
+          Session.set('lightState', data.data.result);
+        });
       if(Session.get('lightState')==1) {
         document.getElementById("lightButton").value="Turn OFF";
       } else {
         document.getElementById("lightButton").value="Turn ON";
       }
       return Session.get('lightState');
-    },
-    device: function() {
-      return Session.get('deviceID');
-    },
-    token: function() {
-      return Session.get('accessToken');
     }
   });
 
   Template.led.events({
     'click button': function () {
-      Meteor.http.post("https://api.particle.io/v1/devices/53ff70066667574834302067/toggleLight?access_token=dcb98d8057f3a74e2234d4a44a80cbfbafb3d183", function (err, data ){
+      var device = Session.get('deviceID');
+      var token = Session.get('accessToken');
+      Meteor.http.post(
+        "https://api.particle.io/v1/devices/" + device +
+        "/toggleLight?access_token=" + token,
+        function (err, data ){
+      // ("https://api.particle.io/v1/devices/53ff70066667574834302067/toggleLight?access_token=dcb98d8057f3a74e2234d4a44a80cbfbafb3d183", function (err, data ){
         console.log("HTTP POST response received");
         console.log(data);
         Session.set('lightState', data.data.result);
